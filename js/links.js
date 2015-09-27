4275ca9f0a9ddc8fcfb373fbfa35656985467c94
dwc3993 <chr11031@byui.edu>
2015-09-26 22:19:13 -0600

    First major upload

---
+++ js/links.js
@@ -0,0 +1,8 @@
+function on(box, text) {
+    document.getElementById(box).style.backgroundColor = "#555555";
+    document.getElementById(text).style.color = "#D9D9D9";
+}
+function off(box, text) {
+    document.getElementById(box).style.backgroundColor = "#E7E7E7";
+    document.getElementById(text).style.color = "#555";
+}
